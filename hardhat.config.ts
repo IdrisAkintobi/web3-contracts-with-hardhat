0882dd9cf582847d751b1cbdd9355eaa9284a32c
Idris Akintobi <akintobiidris@gmail.com>
2024-08-24 00:33:30 +0100

    updated hardhat config

---
+++ hardhat.config.ts
@@ -0,0 +1,16 @@
+import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig, vars } from "hardhat/config";
+
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const config: HardhatUserConfig = {
+  solidity: "0.8.26",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
+};
+
+export default config;
