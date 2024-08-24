68fa8c62d1c3761faed4aaabf8ddd867f2fe74a5
Idris Akintobi <akintobiidris@gmail.com>
2024-08-24 15:04:10 +0100

    changed code compiler version and redeployed

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.26;
 
+// Author: @IdrisAkintobi
 contract Bank {
     string internal bankName = "iDris Web3 Bank";
 
