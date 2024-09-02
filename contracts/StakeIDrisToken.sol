8848b795163414e671aa6fb80588af7144a3c1fb
Idris Akintobi Adejare <akintobiidris@gmail.com>
2024-09-02 13:10:30 +0100

    fixed apy calculation bug

--- contracts/StakeIDrisToken.sol
+++ contracts/StakeIDrisToken.sol
@@ -6,15 +6,22 @@ contract StakeIDrisToken {
     error AddressZeroDetected();
     error ZeroValueNotAllowed();
     error CantSendToZeroAddress();
+    error AssetIsLocked();
     error InsufficientFunds();
     error NoRecordFound();
     error NotOwner();
     error InsufficientContractBalance();
+    error CanNotTransferToExistingStaker();
 
     struct Record {
         uint256 amountStaked;
         uint256 stakedDate;
         uint256 withdrawableDate;
+    }
+
+    struct StakerRecord {
+        Record staker;
+        uint256 interestAccrued;
     }
 
     address immutable owner;
@@ -25,16 +32,12 @@ contract StakeIDrisToken {
     uint256 immutable INTEREST = 5 * 100; // 5% interest expressed as 500 for fixed-point math
     uint256 immutable PRECISION = 1e4; // Precision for fixed-point calculations
 
-    event StakingSuccessful(
-        address indexed user,
-        uint256 indexed amount,
-        uint8 months
-    );
-    event WithdrawalSuccessful(address indexed user, uint256 indexed amount);
+    event StakingSuccessful(address indexed user, uint256 amount, uint8 months);
+    event WithdrawalSuccessful(address indexed user, uint256 amount);
     event TransferSuccessful(
         address indexed from,
         address indexed _to,
-        uint256 indexed amount
+        uint256 amount
     );
 
     constructor(address _tokenAddress) {
@@ -42,19 +45,12 @@ contract StakeIDrisToken {
         token = IERC20(_tokenAddress);
     }
 
-    //TODO: Fix restake bug
     function stake(uint256 _amount, uint8 _months) external {
-        if (msg.sender == address(0)) {
-            revert AddressZeroDetected();
-        }
+        if (msg.sender == address(0)) revert AddressZeroDetected();
 
-        if (_amount <= 0) {
-            revert ZeroValueNotAllowed();
-        }
+        if ((_amount <= 0 || _months <= 0)) revert ZeroValueNotAllowed();
 
-        if (token.balanceOf(msg.sender) < _amount) {
-            revert InsufficientFunds();
-        }
+        if (token.balanceOf(msg.sender) < _amount) revert InsufficientFunds();
 
         token.transferFrom(msg.sender, address(this), _amount);
 
@@ -78,17 +74,82 @@ contract StakeIDrisToken {
         emit StakingSuccessful(msg.sender, stakerRecord.amountStaked, _months);
     }
 
-    function getMonthsStaked() private view returns (uint256) {
+    function getStakeRecord() public view returns (StakerRecord memory) {
+        if (msg.sender == address(0)) revert AddressZeroDetected();
+
         Record memory stakerRecord = records[msg.sender];
-        return
-            (stakerRecord.withdrawableDate - stakerRecord.stakedDate) /
-            A_MONTH_IN_SECONDS;
+        if (stakerRecord.amountStaked == 0) revert NoRecordFound();
+
+        return StakerRecord(stakerRecord, calculateAPY(stakerRecord));
+    }
+
+    function withdraw() external payable {
+        if (msg.sender == address(0)) revert AddressZeroDetected();
+
+        Record storage stakerRecord = records[msg.sender];
+
+        if (stakerRecord.amountStaked == 0) revert NoRecordFound();
+
+        if (stakerRecord.withdrawableDate > block.timestamp)
+            revert AssetIsLocked();
+
+        uint256 apy = calculateAPY(stakerRecord);
+        uint256 totalBalance = stakerRecord.amountStaked + apy;
+
+        stakerRecord.amountStaked = 0;
+
+        token.transfer(msg.sender, totalBalance);
+
+        emit WithdrawalSuccessful(msg.sender, totalBalance);
+    }
+
+    function transferStake(
+        address _to,
+        uint256 _amount,
+        uint8 _months
+    ) external {
+        if (_to == address(0)) revert CantSendToZeroAddress();
+
+        if (_amount <= 0) revert ZeroValueNotAllowed();
+
+        Record storage stakerRecord = records[msg.sender];
+
+        if (_amount > stakerRecord.amountStaked) revert InsufficientFunds();
+
+        if (records[_to].amountStaked > 0)
+            revert CanNotTransferToExistingStaker();
+
+        stakerRecord.amountStaked -= _amount;
+        records[_to] = Record(
+            _amount,
+            block.timestamp,
+            block.timestamp + (_months * A_MONTH_IN_SECONDS)
+        );
+
+        emit TransferSuccessful(msg.sender, _to, _amount);
+    }
+
+    function getContractBalance() external view returns (uint256) {
+        onlyOwner();
+
+        return token.balanceOf(address(this));
+    }
+
+    function ownerWithdraw(uint256 _amount) external {
+        onlyOwner();
+
+        if (_amount > token.balanceOf(address(this))) {
+            revert InsufficientContractBalance();
+        }
+
+        token.transfer(owner, _amount);
     }
 
     function calculateAPY(
         Record memory _record
     ) private view returns (uint256) {
-        uint256 numberOfMonths = getMonthsStaked();
+        uint256 numberOfMonths = (block.timestamp - _record.stakedDate) /
+            A_MONTH_IN_SECONDS;
 
         // If the staking duration is less than a month, return 0
         if (numberOfMonths < 1) {
@@ -104,88 +165,6 @@ contract StakeIDrisToken {
         uint256 interestAccrued = (_record.amountStaked * apy) / PRECISION;
 
         return interestAccrued;
-    }
-
-    function getStakeBalance() public view returns (uint256) {
-        if (msg.sender == address(0)) {
-            revert AddressZeroDetected();
-        }
-
-        Record memory stakerRecord = records[msg.sender];
-        if (stakerRecord.amountStaked == 0) {
-            revert NoRecordFound();
-        }
-
-        return stakerRecord.amountStaked;
-    }
-
-    function withdraw(uint256 _amount) external {
-        if (msg.sender == address(0)) {
-            revert AddressZeroDetected();
-        }
-
-        if (_amount <= 0) {
-            revert ZeroValueNotAllowed();
-        }
-
-        Record storage stakerRecord = records[msg.sender];
-
-        if (stakerRecord.amountStaked == 0) {
-            revert NoRecordFound();
-        }
-
-        uint256 apy = calculateAPY(stakerRecord);
-        uint256 totalBalance = stakerRecord.amountStaked + apy;
-
-        if (_amount > totalBalance) {
-            revert InsufficientFunds();
-        }
-
-        stakerRecord.amountStaked = totalBalance - _amount;
-        stakerRecord.stakedDate = block.timestamp; // Resetting staking date after withdrawal
-        stakerRecord.withdrawableDate =
-            block.timestamp +
-            (stakerRecord.withdrawableDate - stakerRecord.stakedDate);
-
-        token.transfer(msg.sender, _amount);
-
-        emit WithdrawalSuccessful(msg.sender, _amount);
-    }
-
-    function transferStake(address _to, uint256 _amount) external {
-        if (_to == address(0)) {
-            revert CantSendToZeroAddress();
-        }
-
-        if (_amount <= 0) {
-            revert ZeroValueNotAllowed();
-        }
-
-        Record storage stakerRecord = records[msg.sender];
-        if (stakerRecord.amountStaked == 0) {
-            revert NoRecordFound();
-        }
-
-        if (_amount > stakerRecord.amountStaked) {
-            revert InsufficientFunds();
-        }
-
-        stakerRecord.amountStaked -= _amount;
-        records[_to].amountStaked += _amount;
-        records[_to].stakedDate = block.timestamp; // Reset staking date for the new owner
-        records[_to].withdrawableDate = stakerRecord.withdrawableDate;
-
-        emit TransferSuccessful(msg.sender, _to, _amount);
-    }
-
-    function ownerWithdraw(uint256 _amount) external {
-        onlyOwner();
-
-        if (_amount > token.balanceOf(address(this))) {
-            revert InsufficientContractBalance();
-        }
-
-        token.transfer(owner, _amount);
     }
 
     function onlyOwner() private view {
