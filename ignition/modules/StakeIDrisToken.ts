6126857b8c76c66415b504c52c4ddd5d95c88ae6
Idris Akintobi Adejare <akintobiidris@gmail.com>
2024-09-02 13:12:59 +0100

    Deploy StakeIDrisToken contract

--- ignition/modules/StakeIDrisToken.ts
+++ ignition/modules/StakeIDrisToken.ts
@@ -1,7 +1,9 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
+const tokenAddress = "0x39A3bE77b7b7a574800373CE8CaFa7B5082EA82f";
+
 const StakeIDrisTokenModule = buildModule("StakeIDrisTokenModule", (m) => {
-  const StakeIDrisToken = m.contract("StakeIDrisToken");
+  const StakeIDrisToken = m.contract("StakeIDrisToken", [tokenAddress]);
 
   return { StakeIDrisToken };
 });
