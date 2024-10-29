d932d72d592f5bbe04432236221f6c48ae17b259
Idris Akintobi Adejare <akintobiidris@gmail.com>
2024-10-29 21:35:42 +0100

    updated scripts

---
+++ scripts/get-balance.ts
@@ -0,0 +1,19 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  // Get the signer (default account)
+  const [deployer] = await ethers.getSigners();
+
+  // Get the balance using the provider and the signer's address
+  const balance = await ethers.provider.getBalance(deployer);
+
+  const inEther = ethers.formatEther(balance);
+  const in4Decimal = Number(inEther).toFixed(4);
+
+  console.log(`Balance of ${deployer.address}: ${in4Decimal} ETH`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
